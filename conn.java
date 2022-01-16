d18fd439fa0e4d23d0fcdb8b75241e2342bcaaaf
iamriya <riyashinde173@gmail.com>
2022-01-16 18:54:07 +0530

    Hotel management

---
+++ conn.java
@@ -0,0 +1,23 @@
+package hotel.management.system;
+
+
+
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///projecthms","root","root"); 
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
